506669f46f395f7486dfc80ab66527a8eb6a8eb4
nhannt97 <nhannt97hy@gmail.com>
2018-04-07 01:50:27 +0700

    update

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './app.js', 
+        './home/home.js',
+        './pitch/pitch.js',
+        './component/navigation/navigation.js',
+        './component/footer/footer.js'
+    ],
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'main.bundle.js'
+    }
+};
